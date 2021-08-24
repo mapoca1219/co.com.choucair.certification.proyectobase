2c5d71e020bf9e569ce8892a770217b162e571f8
Marco <60860272+mapoca1219@users.noreply.github.com>
2021-08-23 19:48:35 -0500

    Complemento4

--- src/test/java/co/com/choucair/certification/proyectobase/rennuers/RunnerTags.java
+++ src/test/java/co/com/choucair/certification/proyectobase/rennuers/RunnerTags.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions (
         features = {"src/test/resources/features/academyChoucair.feature"},
         tags = "@stories",
-        glue = "co.com.choucair.certificacion.proyectobase.stepdefinitions",
+        glue = {"co.com.choucair.certificacion.proyectobase.stepdefinitions"},
         snippets = SnippetType.CAMELCASE)
 public class RunnerTags{
 
